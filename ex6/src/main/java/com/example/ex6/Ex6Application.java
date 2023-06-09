dc506da79cdf955609b04df2495a34336623764d
adelmnv <99648706+adelmnv@users.noreply.github.com>
2023-06-09 21:57:30 +0600

    Initial commit

--- ex6/src/main/java/com/example/ex6/Ex6Application.java
+++ ex6/src/main/java/com/example/ex6/Ex6Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ex5Application {
+public class Ex6Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Ex5Application.class, args);
+        SpringApplication.run(Ex6Application.class, args);
     }
 
 }
--- ex6/src/test/java/com/example/ex6/Ex6ApplicationTests.java
+++ ex6/src/test/java/com/example/ex6/Ex6ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Ex5ApplicationTests {
+class Ex6ApplicationTests {
 
     @Test
     void contextLoads() {
